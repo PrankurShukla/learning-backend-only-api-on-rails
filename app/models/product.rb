b1c3297c980f9cb31481326f3c34ab39613b81f4
PrankurShukla <shuklaprankur27@gmail.com>
2025-08-25 16:47:46 +0530

    added category modeld and define its relationship with product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
+    belongs_to :category
+    
     validates :name, presence: true
     validates :price, numericality: {greater_than_or_equal_to: 0}
     validates :published, inclusion: { in: [true, false] }
