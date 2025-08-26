140602e32158bb4311231dbf45a8df712bb38124
PrankurShukla <shuklaprankur27@gmail.com>
2025-08-26 13:24:22 +0530

    add description field to prodct model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_08_26_062121) do
+ActiveRecord::Schema[8.0].define(version: 2025_08_26_075305) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -24,6 +24,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_26_062121) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "category_id", null: false
+    t.string "description"
     t.index ["category_id"], name: "index_products_on_category_id"
   end
 
