da9b9db185d1b2a4c35bc07cd9a1d1a79bebac76
PrankurShukla <shuklaprankur27@gmail.com>
2025-08-26 13:07:33 +0530

    fixed code for jsown web token payload

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::API
-
+before_action :authorize_request
 private
     def authorize_request
     header = request.headers["Authorization"]
--- app/services/json_web_token.rb
+++ app/services/json_web_token.rb
@@ -1,7 +1,7 @@
 module JsonWebToken
   SECRET_KEY = Rails.application.secret_key_base
 
-  def self.encode(payload, exp: 24.hours.from_now)
+  def self.encode(payload, exp = 24.hours.from_now)
     payload[:exp] = exp.to_i
     JWT.encode(payload, SECRET_KEY, "HS256")
   end
